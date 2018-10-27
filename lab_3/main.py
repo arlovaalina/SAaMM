2717951484d2edb40c8f42be96c41010806782f9
arlovaalina <arlovaalina@gmail.com>
2018-10-27 19:44:07 +0300

    Start lab 3-4

--- lab_3/main.py
+++ lab_3/main.py
@@ -62,23 +62,23 @@ def initialize_window():
     root = Tk()
     root.geometry("{0}x{1}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
 
-    a_value_label = Label(root, text = "Параметр а").place(x = 10, y = 10)
-    a_value_input = Entry(root)
+    a_value_label = Label(root, text = "Вероятность не выдачи заявки, ρ").place(x = 10, y = 10)
+    a_value_input = Entry(root, width = 27)
     a_value_input.place(x = 10, y = 30)
     
-    r_value_label = Label(root, text = "Параметр r").place(x = 10, y = 60)
-    r_value_input = Entry(root)
+    r_value_label = Label(root, text = "Вероятность просеивания, π1").place(x = 10, y = 60)
+    r_value_input = Entry(root, width = 27)
     r_value_input.place(x = 10, y = 80)
     
-    m_value_label = Label(root, text = "Параметр m").place(x = 10, y = 110)
-    m_value_input = Entry(root)
+    m_value_label = Label(root, text = "Вероятность просеивания, π2").place(x = 10, y = 110)
+    m_value_input = Entry(root, width = 27)
     m_value_input.place(x = 10, y = 130)
     
-    n_value_label = Label(root, text = "Количество чисел").place(x = 10, y = 160)
-    n_value_input = Entry(root)
+    n_value_label = Label(root, text = "Количество тактов").place(x = 10, y = 160)
+    n_value_input = Entry(root, width = 27)
     n_value_input.place(x = 10, y = 180)
     
-    start_button = Button(root, text = "Начать", width = 10,
+    start_button = Button(root, text = "Моделировать", width = 15,
         command = lambda: start_algorithm(root, int(a_value_input.get()), int(r_value_input.get()), int(m_value_input.get()), int(n_value_input.get())))
     start_button.place(x = 10, y = 210)
 
