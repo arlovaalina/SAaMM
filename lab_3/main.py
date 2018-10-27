bace59f916b07a17b7a157b4c20dd917fd3019ae
arlovaalina <alina.orlova@unitedcuisines.com>
2018-10-27 21:03:40 +0300

    Implement graph with states

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,85 +1,56 @@
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+from random import *
 
-from generator import *
 from tkinter import *
 from variables import *
+from stateMatrix import *
 
+def getNewState(graph, currState, probability):
+    prevTransition = 0
+    for i in range(STATE_AMOUNT):
+        transition = prevTransition + graph[currState][i]
+        if probability < transition:
+            return i
+        else:
+            prevTransition = transition
+    
 
-def show_chart(root, numbers):
-    minValue = min(numbers)
-    maxValue = max(numbers)
+def start_algorithm(p, pi1, pi2, tactsAmount):
+    graph = getStateMatrix(p, pi1, pi2)
+    print(graph)
     
-    numbersRange = maxValue - minValue
-    barLength = numbersRange / BARS_IN_CHART
-    
-    frequencies = []
-    intervals = []
-    minBoundary = minValue
-    maxBoundary = minBoundary + barLength
-    
-    for i in range(BARS_IN_CHART):
-        numbersInInterval = list(filter(lambda number: number >= minBoundary and number < maxBoundary, numbers))
-        frequency = len(numbersInInterval) / len(numbers)
-        frequencies.append(frequency)
-        intervals.append(minBoundary)
-        minBoundary += barLength
-        maxBoundary += barLength
-    
-    figure = Figure(figsize = (6, 4), dpi = 100)
-    a = figure.add_subplot(111)
-    a.bar(intervals, frequencies, barLength, color = '#90CAF9', edgecolor = '#1976D2')
-    canvas = FigureCanvasTkAgg(figure, root)
-    canvas.show()
-    canvas.get_tk_widget().place(x = 200, y = 10)
+    currState = 0
+    for tact in range(tactsAmount):
+        print(currState)
+        probability = random()
+        currState = getNewState(graph, currState, probability)
+        print(probability)
 
-
-def start_algorithm(root, a, r, m, numbersAmount):
-    numbers = generate_numbers(a, r, m, numbersAmount)
-    print(numbers)
-    
-    expectation = find_expectation(numbers)
-    dispersion = find_dispersion(numbers, expectation)
-    standard_deviation = find_standard_deviation(dispersion)
-    period = find_period(numbers)
-    aperiodic_interval = find_aperiodic_interval(numbers, period)
-    indirect_indications = find_indirect_indications(numbers)
-    print(indirect_indications)
-    
-    show_chart(root, numbers)
-    
-    expectation_label = Label(root, text = "Мат. Ожидание: " + str(round(expectation, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 420)
-    dispersion_label = Label(root, text = "Дисперсия: " + str(round(dispersion, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 440)
-    standard_deviation_label = Label(root, text = "Среднеквадратическое отклонение: " + str(round(standard_deviation, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 460)
-    period_label = Label(root, text = "Период: " + str(round(period, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 480)
-    aperiodic_interval_label = Label(root, text = "Длина отрезка апериодичности: " + str(round(aperiodic_interval, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 500)
-    indirect_indications_label = Label(root, text = "2К / N: " + str(round(indirect_indications, 3)) + "; pi / 4: " + str(round(math.pi / 4, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 520)
+    # aperiodic_interval_label = Label(root, text = "Длина отрезка апериодичности: " + str(round(aperiodic_interval, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 500)
+    # indirect_indications_label = Label(root, text = "2К / N: " + str(round(indirect_indications, 3)) + "; pi / 4: " + str(round(math.pi / 4, 3)), height = 1, width = 50, anchor = 'w').place(x = 200, y = 520)
     
 
 def initialize_window():
     root = Tk()
     root.geometry("{0}x{1}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
 
-    a_value_label = Label(root, text = "Вероятность не выдачи заявки, ρ").place(x = 10, y = 10)
-    a_value_input = Entry(root, width = 27)
-    a_value_input.place(x = 10, y = 30)
+    p_label = Label(root, text = "Вероятность не выдачи заявки, ρ").place(x = 10, y = 10)
+    p_input = Entry(root, width = 27)
+    p_input.place(x = 10, y = 30)
     
-    r_value_label = Label(root, text = "Вероятность просеивания, π1").place(x = 10, y = 60)
-    r_value_input = Entry(root, width = 27)
-    r_value_input.place(x = 10, y = 80)
+    pi1_label = Label(root, text = "Вероятность просеивания, π1").place(x = 10, y = 60)
+    pi1_input = Entry(root, width = 27)
+    pi1_input.place(x = 10, y = 80)
     
-    m_value_label = Label(root, text = "Вероятность просеивания, π2").place(x = 10, y = 110)
-    m_value_input = Entry(root, width = 27)
-    m_value_input.place(x = 10, y = 130)
+    pi2_label = Label(root, text = "Вероятность просеивания, π2").place(x = 10, y = 110)
+    pi2_input = Entry(root, width = 27)
+    pi2_input.place(x = 10, y = 130)
     
-    n_value_label = Label(root, text = "Количество тактов").place(x = 10, y = 160)
-    n_value_input = Entry(root, width = 27)
-    n_value_input.place(x = 10, y = 180)
+    tacts_label = Label(root, text = "Количество тактов").place(x = 10, y = 160)
+    tacts_input = Entry(root, width = 27)
+    tacts_input.place(x = 10, y = 180)
     
     start_button = Button(root, text = "Моделировать", width = 15,
-        command = lambda: start_algorithm(root, int(a_value_input.get()), int(r_value_input.get()), int(m_value_input.get()), int(n_value_input.get())))
+        command = lambda: start_algorithm(float(p_input.get()), float(pi1_input.get()), float(pi2_input.get()), int(tacts_input.get())))
     start_button.place(x = 10, y = 210)
 
     root.mainloop()
